0ae3fefb36790b7dcbae990ba5e4a3fe27e16d69
tunde <omojuwababatunde1@gmail.com>
2023-06-06 15:37:55 -0700

    task 1 added

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-reminder = abs(number % 10)
+reminder = abs(number) % 10
 if number < 0:
     reminder = -reminder
-output = ''
 if reminder == 0:
     output = "0"
 elif reminder > 5:
