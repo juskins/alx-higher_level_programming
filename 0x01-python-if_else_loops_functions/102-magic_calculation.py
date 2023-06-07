cf9b71209364a09d4e980172e077eda27fb34d1c
tunde <omojuwababatunde1@gmail.com>
2023-06-06 18:31:08 -0700

    task 102 updated

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
     if a < b:
-        return c
-    elif a >= b:
-        if b > c:
-            return a + b
-        else:
-            return (a * b) - c
+        return (c)
+    elif c > b:
+        return (a + b)
+    else:
+         return ((a * b) - c)
 
