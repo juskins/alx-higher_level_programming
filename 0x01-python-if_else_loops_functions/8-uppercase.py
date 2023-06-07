76b268343f757b5f333cb648eaec99dbe24d5bdd
tunde <omojuwababatunde1@gmail.com>
2023-06-06 17:02:00 -0700

    task 8 added

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def isupper(str):
+    for character in str:
+        if ord('a') <= ord(character) <= ord('z'):
+            print("{:s}".format(chr(ord(character) - 32)), end="")
+        else:
+            print("{:s}".format(character), end="")
+    print("")
