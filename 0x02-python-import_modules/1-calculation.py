28e184d4a8c0e3a5cf09c91672d0cf2c3072753f
tunde <omojuwababatunde1@gmail.com>
2023-06-08 20:24:36 -0700

    task 1 added

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from calculate_1 import add, sub, div, mul
+a = 10
+b = 5
+print("{0} + {1} = {2}".format(a, b, add(a,b)))
+print("{0} - {1} = {2}".format(a, b, sub(a,b)))
+print("{0} / {1} = {2}".format(a, b, div(a,b)))
+print("{0} * {1} = {2}".format(a, b, mul(a,b)))
