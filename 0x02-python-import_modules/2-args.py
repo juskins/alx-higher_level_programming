43e15829534cbf12caf08da48b6aba33b4770b7b
tunde <omojuwababatunde1@gmail.com>
2023-06-12 08:57:00 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-argv_count = len(sys.argv) - 1
-if argv_count == 0:
-    print("0 argument")
-elif argv_count == 1:
-    print("1 argument")
-else print("{} arguments".format(argv_count))
-for j in range(argv_count):
-    print("{} {}".format(j + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    argv_count = len(sys.argv) - 1
+    if argv_count == 0:
+        print("0 argument")
+    elif argv_count == 1:
+        print("1 argument")
+    else print("{} arguments".format(argv_count))
+    for j in range(argv_count):
+        print("{} {}".format(j + 1, sys.argv[i + 1]))
