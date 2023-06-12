5cbe833cfd4b9a84929604ab6bac5ba7e9fb75ba
tunde <omojuwababatunde1@gmail.com>
2023-06-12 09:40:28 -0700

    task 102 updated

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         k = add(a, b)
-        for e in range(4, 6)
-        k = add(k, e)
+        for e in range(4, 6):
+            k = add(k, e)
         return k
     else:
         return (sub(a, b))
