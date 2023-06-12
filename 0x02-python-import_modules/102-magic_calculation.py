e154f67c2f704a11331f96e075d0d4c3719ed1f2
tunde <omojuwababatunde1@gmail.com>
2023-06-12 09:37:10 -0700

    task 102 added

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if i < j:
-        k = add(i, j)
-        for a in range(4, 6)
-        k = add(k, a)
+    if a < b:
+        k = add(a, b)
+        for e in range(4, 6)
+        k = add(k, e)
         return k
     else:
-        return (sub(i, j))
+        return (sub(a, b))
